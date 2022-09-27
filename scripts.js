1cc54d4e5a6e5971117ffe1ce1d99aaa7f8e905e
leandrocodes <leandrovianacodes@gmail.com>
2022-09-26 21:50:36 -0400

    chore: new color scheme

--- scripts.js
+++ scripts.js
@@ -18,11 +18,11 @@ window.onscroll = () => {
 const darkmode = document.querySelector('#darkmode')
 
 darkmode.onclick = () => {
-	if (darkmode.classList.contains('bx-moon')) {
-		darkmode.classList.replace('bx-moon', 'bx-sun')
+	if (darkmode.classList.contains('bx-sun')) {
+		darkmode.classList.replace('bx-sun', 'bx-moon')
 		document.body.classList.add('active')
 	} else {
-		darkmode.classList.replace('bx-sun', 'bx-moon')
+		darkmode.classList.replace('bx-moon', 'bx-sun')
 		document.body.classList.remove('active')
 	}
 }
