94923aa786096c855bccc70458bc2557db8f2ea0
leandrocodes <leandrovianacodes@gmail.com>
2025-08-24 18:51:37 -0400

    fix: gh actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/', // Como você tem domínio personalizado (leandrocodes.com)
+  base: '/',
   build: {
     outDir: 'dist',
   }
