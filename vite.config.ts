b0e8242501e1f7a8586dddb14ac4ffc151511d7e
leandrocodes <leandrovianacodes@gmail.com>
2025-08-25 10:12:44 -0400

    fix: gh actions

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,15 @@ export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  },
+  esbuild: {
+    target: 'es2015'
   }
 })
